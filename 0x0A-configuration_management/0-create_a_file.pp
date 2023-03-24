6f89120b28c63b69ee8d71baff4ae88e508d96f9
Cyriacusifeco <cyriacusifeco@gmail.com>
2023-03-24 18:08:39 +0000

    create a file with puppet

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,7 @@
+# Create a file in the /tmp directory named school
+
+file {'/tmp/school':
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet'}
---
+++ 0x0A-configuration_management/README.md
@@ -0,0 +1,1 @@
+SySADMIN - Configuration management
